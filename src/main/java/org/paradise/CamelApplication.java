2af194d2b91b27bb5c242f0828c3e4dc26f35cf4
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-01-19 22:45:57 +1100

    Add the skeleton of Spring Boot project

---
+++ src/main/java/org/paradise/CamelApplication.java
@@ -0,0 +1,13 @@
+package org.paradise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/paradise/CamelApplicationTests.java
@@ -0,0 +1,16 @@
+package org.paradise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CamelApplication.class)
+public class CamelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
