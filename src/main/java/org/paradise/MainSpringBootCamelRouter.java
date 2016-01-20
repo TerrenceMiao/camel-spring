156a43b635da711b46a62bea610ce5d6f44b0b7e
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-01-20 18:18:55 +1100

    Add a simple Camel router

--- src/main/java/org/paradise/MainSpringBootCamelRouter.java
+++ src/main/java/org/paradise/MainSpringBootCamelRouter.java
@@ -1,13 +1,29 @@
 package org.paradise;
 
+import org.apache.camel.spring.boot.FatJarRouter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MainSpringBootCamelRouter {
+public class MainSpringBootCamelRouter extends FatJarRouter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainSpringBootCamelRouter.class, args);
+	}
+
+	@Override
+	public void configure() {
+
+		from("timer:trigger")
+				.transform().simple("ref:myBean")
+				.to("log:out");
+	}
+
+	@Bean
+	String myBean() {
+
+		return "This is a Spring bean!";
 	}
 
 }
---
+++ src/main/java/org/paradise/MainSpringBootCamelRouterWarInitializer.java
@@ -0,0 +1,16 @@
+package org.paradise;
+
+import org.apache.camel.spring.boot.FatJarRouter;
+import org.apache.camel.spring.boot.FatWarInitializer;
+
+/**
+ * Created by miaot on 20/01/2016.
+ */
+public class MainSpringBootCamelRouterWarInitializer extends FatWarInitializer {
+
+    @Override protected Class<? extends FatJarRouter> routerClass() {
+
+        return MainSpringBootCamelRouter.class;
+    }
+    
+}
