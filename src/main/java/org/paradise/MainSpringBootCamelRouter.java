3d1d1214f493154dfc3050750e17e49313a8fd7e
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-01-21 23:48:21 +1100

    Enable Hawtio plugin and disable authentication when access http://localhost:8080/hawtio/index.html

--- src/main/java/org/paradise/MainSpringBootCamelRouter.java
+++ src/main/java/org/paradise/MainSpringBootCamelRouter.java
@@ -1,14 +1,20 @@
 package org.paradise;
 
+import io.hawt.springboot.EnableHawtio;
+import io.hawt.web.AuthenticationFilter;
 import org.apache.camel.spring.boot.FatJarRouter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableHawtio
 public class MainSpringBootCamelRouter extends FatJarRouter {
 
 	public static void main(String[] args) {
+
+		System.setProperty(AuthenticationFilter.HAWTIO_AUTHENTICATION_ENABLED, "false");
+
 		SpringApplication.run(MainSpringBootCamelRouter.class, args);
 	}
 
