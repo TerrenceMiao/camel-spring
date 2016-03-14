05b2441111c4a5db299bff408e6e6d598ba1428b
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-03-14 16:26:19 +1100

    Refactor Person class

--- src/main/java/org/paradise/domain/Person.java
+++ src/main/java/org/paradise/domain/Person.java
@@ -1,5 +1,8 @@
 package org.paradise.domain;
 
+import org.paradise.exception.NullValueException;
+import org.paradise.monad.Try;
+
 import java.util.Optional;
 
 /**
@@ -12,12 +15,22 @@ public class Person {
 
     private static Optional<Car> car;
 
+    private static Address address;
+
+
     public Person(Car car) {
         this.car = Optional.ofNullable(car);
     }
 
     public static <U> Optional<Car> getCar(U u) {
         return car;
+    }
+
+    public static <U> Try<Address> getAddress(U u) {
+
+        return address == null
+                ? Try.failure(u.toString(), new NullValueException("Address is NULL"))
+                : Try.success(address);
     }
 
     public String getName() {
@@ -34,6 +47,16 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person {" +
+                "name = [" + name + ']' +
+                ", age = [" + age + ']' +
+                ", car = [" + car + ']' +
+                ", address = [" + address + ']' +
+                '}';
     }
 
 }
