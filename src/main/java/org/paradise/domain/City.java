0b702f614d8a18ee1a0a9a3c6f4521d18f683fc2
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-03-14 16:25:12 +1100

    Add City class

---
+++ src/main/java/org/paradise/domain/City.java
@@ -0,0 +1,19 @@
+package org.paradise.domain;
+
+/**
+ * Created by terrence on 14/03/2016.
+ */
+public class City {
+
+    private static String name;
+
+    public City(String name) {
+
+        this.name = name;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+}
