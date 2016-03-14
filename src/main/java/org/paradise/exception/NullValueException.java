cd5a93b615053094efaf63756313f5d30ea1eff5
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-03-14 16:21:24 +1100

    Add more Exception constructors

--- src/main/java/org/paradise/exception/NullValueException.java
+++ src/main/java/org/paradise/exception/NullValueException.java
@@ -5,4 +5,23 @@ package org.paradise.exception;
  */
 public class NullValueException extends Exception {
 
+    public NullValueException() {
+    }
+
+    public NullValueException(String message) {
+        super(message);
+    }
+
+    public NullValueException(Throwable cause) {
+        super(cause);
+    }
+
+    public NullValueException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NullValueException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
 }
