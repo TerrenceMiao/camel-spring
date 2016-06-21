0fe4998a3571eb08d412ecb0534045a4ea0b5d10
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-06-22 09:31:01 +1000

    Add @EnableFeignClients annotation

--- src/main/java/org/paradise/MainSpringBootCamelRouter.java
+++ src/main/java/org/paradise/MainSpringBootCamelRouter.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
@@ -19,6 +20,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableHawtio
 @EnableDiscoveryClient
+@EnableFeignClients
 @EnableCircuitBreaker
 @EnableHystrix
 @EnableHystrixDashboard
