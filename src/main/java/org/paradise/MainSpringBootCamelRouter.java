bbe0077dc2312c2014091fdae33c04a8b99f2082
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-06-26 22:01:26 +1000

    Add how to use Zipkin Tracing

--- src/main/java/org/paradise/MainSpringBootCamelRouter.java
+++ src/main/java/org/paradise/MainSpringBootCamelRouter.java
@@ -10,8 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -22,8 +20,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCircuitBreaker
-@EnableHystrix
-@EnableHystrixDashboard
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableAsync
 public class MainSpringBootCamelRouter extends FatJarRouter {
