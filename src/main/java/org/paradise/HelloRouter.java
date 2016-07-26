435463250a4406bbc380fb132cdc28205edc3dd7
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-07-26 22:56:40 +1000

    Add REST service support integrated with Apache Camel

---
+++ src/main/java/org/paradise/HelloRouter.java
@@ -0,0 +1,38 @@
+package org.paradise;
+
+import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.spring.boot.FatJarRouter;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by terrence on 26/07/2016.
+ */
+@Component
+public class HelloRouter extends FatJarRouter {
+
+    @Override
+    public void configure() {
+
+        // configure to use jetty on localhost with the given port and enable auto binding mode
+        restConfiguration()
+                .component("jetty")
+                .host("0.0.0.0").port(9090)
+                .bindingMode(RestBindingMode.json)
+                .dataFormatProperty("json.in.disableFeatures", "FAIL_ON_UNKNOWN_PROPERTIES, ADJUST_DATES_TO_CONTEXT_TIME_ZONE")
+                .dataFormatProperty("json.in.enableFeatures", "FAIL_ON_NUMBERS_FOR_ENUMS, USE_BIG_DECIMAL_FOR_FLOATS")
+                .apiContextPath("/api-doc")
+                .apiProperty("api.title", "API")
+                .apiProperty("api.version", "1.0.0")
+                .apiProperty("cors", "true");
+
+        rest("/say")
+                .description("Say hello")
+                .consumes("application/json")
+                .produces("application/json")
+                .get("/hello")
+                .route()
+                .routeId("Hello REST")
+                .transform().constant("Say Hello REST!");
+    }
+
+}
